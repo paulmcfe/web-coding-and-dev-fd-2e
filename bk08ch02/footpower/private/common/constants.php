75da3df0019accdd0786a8952fde00130ff7ccbb
Paul McFedries <mail@mcfedries.com>
2023-09-11 14:41:28 -0400

    Uploaded Book 8, Chapter 2 examples

--- bk08ch02/footpower/private/common/constants.php
+++
@@ -1,6 +0,0 @@
-<?php
-    define('HOST', 'localhost');
-    define('USER', 'root');
-    define('PASSWORD', '');
-    define('DATABASE', 'footpower');
-?>
--- bk08ch02/footpower/public/common/bottom.php
+++
@@ -1,8 +0,0 @@
-    </main>
-    <footer role="contentinfo">
-        Copyright <?php echo date('Y'); ?> Logophilia Limited
-    </footer>
-    <script src="/js/data.js"></script>
-    <script src="/js/user.js"></script>
-</body>
-</html>
